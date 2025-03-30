158fec205c779d871030b90edc35a7ba9d37d219
hrishi0102 <hrishi0102business@gmail.com>
2025-03-31 02:32:03 +0530

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -42,14 +42,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(morgan('dev'));
-const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: { message: 'Too many requests from this IP, please try again after 15 minutes' }
-});
-app.use('/api/', apiLimiter);
+
 
 // Routes
 app.get('/', (req, res) => {
